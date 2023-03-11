ea46e411b3b6d601884ed4ce18bcaf1c60efd5fe
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-03-11 15:53:59 +0900

    second commit

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -10,17 +10,14 @@ const Introduction = (props) => {
 				Thank you for participating in our experiment.
 			</p>
 			<p>
-				In the study, you will see a series of 64 monochrome scatterplots and will be asked to conduct two tasks for each scatterplot.
+				In the study, you will see a series of 24 monochrome scatterplots and will be asked to conduct a single task.
 			</p>
 			<p>
-				Task 1: We will ask you to find the clusters in the scatterplot. Specifically, we will ask you to <b>lasso the clusters</b> you find using the mouse. Each lasso will represent a cluster.
+				We will ask you to find the clusters in the scatterplot. Specifically, we will ask you to <b>lasso the clusters</b> you find using the mouse. Each lasso will represent a cluster.
 				For the lasso process- click, move around the cluster and click to stop.
 			</p>
 			<p>
-				Task 2: We will ask you to <b>explicitly determine the ambiguity</b> of the scatterplot, which can be interpreted as the difficulty of the lassoing task. Please select your response from -- very clear, clear, neutral, ambiguous, or very ambiguous.
-			</p>	
-			<p>
-				The experiment will last about 60 minutes (one hour). You will see 64 scatterplots, where the first four scatterplots will be included in the practice session. There will be a short interview after the experiment.
+				The experiment will last less than 30 minutes (one hour). You will see 24 scatterplots, where the first four scatterplots will be included in the practice session. There will be a short interview after the experiment.
 			</p>
 			<p>
 				We will also collect participants' non-identifying demographic information.
